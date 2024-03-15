956f87eda33f77cc62e89e10dd906c8e5fd59348
123kamatcho <93836498+123kamatcho@users.noreply.github.com>
2024-03-15 02:50:34 +0200

    push

---
+++ lib/modules/id.dart
@@ -0,0 +1,107 @@
+import 'package:blessmate/modules/logres.dart';
+import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
+import 'dart:io';
+import 'package:image_picker/image_picker.dart';
+
+class MyPage extends StatefulWidget {
+  @override
+  _MyPageState createState() => _MyPageState();
+}
+
+class _MyPageState extends State<MyPage> {
+
+  late String? pickedImagePath = null;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Padding(
+        padding: const EdgeInsets.all(30.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            GestureDetector(
+              onTap: () async {
+                final pickedFile = await ImagePicker().getImage(source: ImageSource.gallery);
+                if (pickedFile != null) {
+                  setState(() {
+                    pickedImagePath = pickedFile.path;
+                  });
+                }
+              },
+              child: Container(
+                width: 180,
+                height: 180,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                    shape: BoxShape.rectangle,
+                  borderRadius: BorderRadius.circular(20),
+                    border: Border.all(
+                      color:Colors.black,
+                      width: 3,
+                    ),
+                ),
+                child: Center(
+                  child: Icon(
+                    Icons.cloud_upload_outlined,
+                    size: 60,
+
+
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            if (pickedImagePath != null)
+              Image.file(
+                // Display the picked image
+                File(pickedImagePath!),
+                width: 100,
+                height: 100,
+              ),
+            SizedBox(
+                height: 20
+            ),
+            Text(
+                "قم برفع ملف به صورة للبطاقة الشخصية و ما يثبت أنك معالج نفسي يمارس المهنة",
+              style: Theme.of(context).textTheme.bodyText1,
+            ),
+            SizedBox(
+                height: 40
+            ),
+            Container(
+              width: double.infinity,
+              height: 50,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadiusDirectional.circular(17),
+                color: HexColor('00B4D8'),
+              ),
+              child: MaterialButton(
+                onPressed: () {
+                  // Add your logic to save the picked image
+                  if (pickedImagePath != null) {
+                    print('Image saved: $pickedImagePath');
+                    // Add your save logic here
+                  }
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => LogRes(),));
+                },
+                child: const Text(
+                  "اشتراك",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 22,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/modules/signindoctor.dart
@@ -0,0 +1,101 @@
+import 'package:blessmate/modules/signup.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
+
+import 'id.dart';
+import 'logres.dart';
+
+class SignInDoc extends StatelessWidget {
+  const SignInDoc({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body:Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Center(
+            child: Image(
+              image: AssetImage('assets/images/img_7.png'),
+              height: 250,
+              fit: BoxFit.cover,
+            ),
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          Text(
+            "اختر فئتك",
+            style: Theme.of(context).textTheme.bodyText1,
+            textAlign: TextAlign.end,
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Container(
+            height: 50,
+            width: 350,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              color: HexColor('00B4D8'),
+              shape: BoxShape.rectangle,
+            ),
+            child: TextButton(
+              child: Text(
+                "معالج نفسي",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 22,
+                ),
+              ),
+              onPressed: () =>
+                  Navigator.pushReplacement(context,
+                    MaterialPageRoute(builder: (context) => MyPage(),
+                    ),
+                  ),
+            ),
+          ),
+          const SizedBox(
+            height: 15.0,
+          ),
+          Container(
+            height: 50,
+            width: 350,
+            clipBehavior:Clip.hardEdge ,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              color:Colors.white,
+              shape: BoxShape.rectangle,
+              border: Border.all(
+                color:HexColor('00B4D8'),
+                width: 2,
+              ),
+            ),
+            child: TextButton(
+              child: Text(
+                "مريض",
+                style: TextStyle(
+                  color: HexColor('00B4D8'),
+                  fontWeight: FontWeight.bold,
+                  fontSize: 22,
+                ),
+              ),
+              onPressed: () =>
+                  Navigator.pushReplacement(context,
+                    MaterialPageRoute(
+                      builder: (context) => LogRes(),
+                    ),
+                  ),
+            ),
+          ),
+
+        ],
+      ),
+
+    );
+  }
+}
